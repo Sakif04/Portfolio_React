2a8ddbccfa157b628c2c1911c58051b79299225d
Sakif Rafid <sakifrafid04@gmail.com>
2022-06-18 23:28:21 +0600

    changed

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AiOutlineEye} from 'react-icons/ai';
 import {FiGithub} from 'react-icons/fi';
+import quiz from "../../images/projects_img/quiz_1.png"
 function Projects() {
   return (
   <>  
@@ -11,7 +12,7 @@ function Projects() {
         <h1 className="project_name">
           Quiz App
         </h1>
-        <img src="../../images/projects_img/quiz_1.png" alt="Quiz App"  />
+        <img src={quiz} alt="Quiz App"  />
         <div className="flex justify-center">
           <a href="https://github.com/Sakif04/Quiz_App/"> <FiGithub  className='link'/></a>
           <a href="https://sakifsquizapp.netlify.app/"> <AiOutlineEye className='link'/></a>
