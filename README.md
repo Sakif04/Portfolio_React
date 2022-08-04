af0a724be81f9cd5af84b14e3f546cd95724034a
Sakif Rafid <sakifrafid04@gmail.com>
2022-08-05 00:48:08 +0600

    changed styling of project page

---
+++ README.md
@@ -0,0 +1,9 @@
+# Portfolio Site made with React
+Go to the link of the portfolio [Preview link](https://sakif04.netlify.app/)
+
+## Attributes of the WebApp
+1. used React Router to create different pages
+2. attractive design with simple navigation
+3. mobile responsive along with desktop
+4. showcased Projects in a particular page
+
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import noteImg from '../../images/projects_img/Note_img.png'
+import noteImg from '../../images/projects_img/Note_img.png';
 import quizImg from '../../images/projects_img/quiz.png';
 import Project from './Project';
 
@@ -9,9 +9,9 @@ function Projects() {
   <>
     <h1 className="projects_header"> Projects</h1>
     <div className='projects' id='projects'>
-      <Project gitLink={"https://github.com/Sakif04/Quiz_App/"} 
-        prevLink={"https://sakifsquizapp.netlify.app/"}  
-        imgLink={quizImg} />
+      <Project imgLink={quizImg}
+      gitLink={"https://github.com/Sakif04/Quiz_App/"} 
+        prevLink={"https://sakifsquizapp.netlify.app/"}  />
       <Project 
         imgLink={noteImg}
         gitLink="https://github.com/Sakif04/React_Note" 
